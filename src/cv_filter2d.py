d6036969ed15868e8af5bd7d5975a01ffbb10ffe
daheung <saasaa001@naver.com>
2025-09-18 15:28:03 +0900

    [2025-09-18] homework

--- src/cv_filter2d.py
+++ src/cv_filter2d.py
@@ -2,26 +2,32 @@ import cv2
 import sys
 import numpy as np
 
+KERNEL_SIZE = 3
+
 def identity_kernel() -> np.array:
     # TODO: 아이덴티티 커널을 정의하세요.
-    arr = [[]]
+    arr = np.identity(KERNEL_SIZE, np.float32)
     kernel = np.array(arr, dtype=np.float32)
     return kernel
 
 def ones_kernel() -> np.array:
     # TODO: 모든 값이 1인 커널을 정의 하세요.
-    arr = [[]]
+    arr = np.ones((KERNEL_SIZE, KERNEL_SIZE), np.float32)
     kernel = np.array(arr, dtype=np.float32)
     return kernel
 
 def original_kernel() -> np.array:
     # TODO: 원본을 그대로 반환하는 커널을 작성 하세요.
-    arr = [[]]
+    arr = np.zeros((KERNEL_SIZE, KERNEL_SIZE), dtype=np.float32)
+    arr[1][1] = 1
+
     kernel = np.array(arr, dtype=np.float32)
     return kernel
 
 def doubling_kernel() -> np.array:
     # TODO: 원본을 2배로 반환 하는 커널을 작성하세요.
-    arr = [[]]
+    arr = np.zeros((KERNEL_SIZE, KERNEL_SIZE), dtype=np.float32)
+    arr[1][1] = 2
+
     kernel = np.array(arr, dtype=np.float32)
     return kernel
